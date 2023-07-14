837c3e962f33fd724b7fac2c6ca334b6bef4fe5b
Matthias Monschein <monschein.matthias@gmail.com>
2023-07-14 21:37:30 +0200

    :construction: small adjustment in extract script

--- asm2sh.sh
+++ asm2sh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # credit to https://www.commandlinefu.com/commands/view/6051/get-all-shellcode-on-binary-file-from-objdump
-objdump -d "$1" | grep '[0-9a-f]:' | grep -v 'file' | cut -f2 -d: | cut -f1-6 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//g' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/g'
+objdump -d "$1" | grep '[0-9a-f]:' | grep -v 'file' | cut -f2 -d: | cut -f1-9 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//g' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/g'
